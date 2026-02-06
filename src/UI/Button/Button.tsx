958ad5072ae2c0e2f3516d2f19d2d9f2d8fc166e
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-07 01:10:05 +0600

    add Button UI component

---
+++ src/UI/Button/Button.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Button.css';
+
+interface IButtonProps extends React.PropsWithChildren {
+  type?: 'submit' | 'reset' | 'button';
+  text?: string;
+  className?: string;
+  style?: React.CSSProperties;
+  onClick?: React.MouseEventHandler;
+  title?: string;
+  disabled?: boolean;
+}
+
+const Button: React.FC<IButtonProps> = React.memo(
+  ({
+    type = 'button',
+    text,
+    className,
+    style,
+    onClick,
+    children,
+    title,
+    disabled,
+  }) => {
+    return (
+      <button
+        type={type}
+        className={className}
+        style={style}
+        onClick={onClick}
+        title={title}
+        disabled={disabled}
+      >
+        {text}
+        {children}
+      </button>
+    );
+  },
+  (prevProps, nextProps) => prevProps.text === nextProps.text,
+);
+
+export default Button;
