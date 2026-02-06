b2716bbc0b634da39904c0b6d9d238ec03d3227c
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-07 02:07:20 +0600

    update Loader styles to be responsive to parent size

--- src/UI/Loader/Loader.tsx
+++ src/UI/Loader/Loader.tsx
@@ -1,6 +1,8 @@
+import './Loader.css';
+
 const Loader = () => {
   return (
-    <img src="/src/assets/gif/loader/pizza_loader.gif" alt="pizza loader" />
+    <img src="/src/assets/gif/loader/pizza_loader.gif" alt="pizza loader" className='pizza-loader'/>
   );
 };
 
