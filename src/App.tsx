2c8b6fd3ef81c49a59b58786a599f197800ddf6f
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-07 03:46:26 +0600

    add routring with MainLayout and AdminLayout including nested admin routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
+import AdminLayout from './layouts/AdminLayout/layout/AdminLayout';
+import AdminPanel from './pages/admin/AdminPanel';
+import MainLayout from './layouts/MainLayout/layout/MainLayout';
+import Home from './containers/dishes/Home/Home';
+import AdminDishes from './pages/admin/AdminDishes/AdminDishes';
+import AdminOrderDishes from './pages/admin/AdminOrderDishes/AdminOrderDishes';
+import AdminAddDish from './pages/admin/AdminAddDish/AdminAddDish';
+
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+
+        <Route element={<AdminLayout />}>
+          <Route path="/admin" element={<AdminPanel />}>
+            <Route path="dishes" element={<AdminDishes />} />
+            <Route path="dishes/add" element={<AdminAddDish />} />
+            <Route path="orders" element={<AdminOrderDishes />} />
+          </Route>
+        </Route>
+      </Routes>
+    </>
   );
 };
 
