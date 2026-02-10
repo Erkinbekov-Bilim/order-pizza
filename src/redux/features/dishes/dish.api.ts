19d5b3b6f9297b83692604f46f27009bb68ae7b5
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-10 22:15:58 +0600

    fetch and normalize dishes list from API

--- src/redux/features/dishes/dish.api.ts
+++ src/redux/features/dishes/dish.api.ts
@@ -1,10 +1,35 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import type { IDishMutation } from '../../../types/dish/dish-mutation';
 import axiosAPI from '../../../api/axiosApi';
+import type { IDish } from '../../../types/dish/dish';
+import type { IDishApi } from '../../../types/dish/dish-api';
 
 export const postNewDish = createAsyncThunk<void, IDishMutation>(
   'dish/postNewDish',
   async (dish) => {
     axiosAPI.post<IDishMutation>('dishes.json', dish);
+  },
+);
+
+export const getDishes = createAsyncThunk<IDish[]>(
+  'dish/getDishes',
+  async () => {
+    const response = await axiosAPI.get<IDishApi>('/dishes.json');
+    const dishesData = response.data;
+
+    if (dishesData) {
+      const dishesIDS: string[] = Object.keys(dishesData);
+
+      const dishes: IDish[] = dishesIDS.map((id) => {
+        return {
+          ...dishesData[id],
+          id,
+        };
+      });
+
+      return dishes;
+    }
+
+    return [];
   },
 );
