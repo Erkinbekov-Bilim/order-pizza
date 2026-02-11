1ace292944107ab46db1f40603445fbb06dde5ff
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-11 22:08:15 +0600

    add DishesCard component with formatted data and dish display

---
+++ src/pages/admin/AdminDishes/DishesCard/DishesCard.tsx
@@ -0,0 +1,44 @@
+import { type FC } from 'react';
+import './DishesCard.css';
+import type { IDish } from '../../../../types/dish/dish';
+import Button from '../../../../UI/Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
+interface IDishCardProps {
+  dish: IDish;
+}
+
+const DishesCard: FC<IDishCardProps> = ({ dish }) => {
+  return (
+    <>
+      <div className="dish-card">
+        <div className="dish-image-block">
+          <img src={dish.image} alt={dish.title} className="dish-image" />
+        </div>
+        <div className="dish-info">
+          <p className="dish-name">{dish.title}</p>
+          <p className="dish-price">{dish.price} KGS</p>
+        </div>
+        <div className="dish-card-actions">
+          <Link
+            to={'edit'}
+            className="dish-card-action dish-card-action-edit"
+            title="Edit"
+          >
+            <FontAwesomeIcon icon={faPen} />
+          </Link>
+          <Button
+            className="dish-card-action dish-card-action-delete"
+            title="Delete"
+          >
+            <FontAwesomeIcon icon={faTrash} />
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DishesCard;
