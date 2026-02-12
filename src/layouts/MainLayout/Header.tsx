de5a80dc1f63d0792a888e4f38b438c06ef73996
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-12 19:39:40 +0600

    wrap header content with Container. Add background for header

--- src/layouts/MainLayout/Header.tsx
+++ src/layouts/MainLayout/Header.tsx
@@ -1,10 +1,16 @@
+import Logo from '../../UI/Logo/Logo';
+import Container from '../Container/Container';
 import '../Layout.css';
 
 const Header = () => {
   return (
     <>
-      <header className="header">
-        <div className="header-content"></div>
+      <header className="header header-main">
+        <Container>
+          <div className="header-content">
+            <Logo to="/" size="medium" />
+          </div>
+        </Container>
       </header>
     </>
   );
