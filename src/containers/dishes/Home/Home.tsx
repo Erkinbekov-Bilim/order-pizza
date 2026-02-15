4195e5b1dec69e9935f0ff48ce15b5d613fb9160
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-15 23:57:49 +0600

    add checkout modal with open/close logic

--- src/containers/dishes/Home/Home.tsx
+++ src/containers/dishes/Home/Home.tsx
@@ -12,15 +12,24 @@ import {
 import Button from '../../../UI/Button/Button';
 import Loader from '../../../UI/Loader/Loader';
 import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getDishes } from '../../../redux/features/dishes/dish.api';
 import './Home.css';
+import DishCart from './DishCart/DishCart';
+import { addDishToCart } from '../../../redux/features/dishes/dishCart/dishCart.slice';
+import Backdrop from '../../../UI/Backdrop/Backdrop';
+import Modal from '../../../UI/Modal/Modal';
 
 const Home = () => {
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
   const dispatch = useAppDispatch();
   const dishes = useAppSelector(selectDishes);
   const { fetchLoading } = useAppSelector(selectLoading);
   const isError = useAppSelector(selectIsError);
+
+  const toggleModal = () => {
+    setIsOpenModal((prev) => !prev);
+  };
 
   useEffect(() => {
     dispatch(getDishes());
@@ -38,7 +47,7 @@ const Home = () => {
     if (!fetchLoading && dishes.length === 0) {
       return (
         <div className="absolute-position-center">
-          <p className="not-found-message">Add dish to see them here</p>
+          <p className="not-found-message">Sorry, we're out of pizza</p>
         </div>
       );
     }
@@ -58,6 +67,7 @@ const Home = () => {
             <Button
               className="dish-card-action dish-card-action-add-cart"
               title="Choose"
+              onClick={() => dispatch(addDishToCart(dish))}
             >
               <p className="add-cart-text">choose</p>
               <FontAwesomeIcon icon={faCartArrowDown} />
@@ -71,7 +81,18 @@ const Home = () => {
   return (
     <>
       <div className="main-dishes">
+        <Button className="nav-btn-checkout" onClick={toggleModal}>
+          checkout
+        </Button>
         <div className="main-dishes-content">{renderContent()}</div>
+        {isOpenModal && (
+          <>
+            <Backdrop onClose={toggleModal} />
+            <Modal onClose={toggleModal}>
+              <DishCart />
+            </Modal>
+          </>
+        )}
       </div>
     </>
   );
