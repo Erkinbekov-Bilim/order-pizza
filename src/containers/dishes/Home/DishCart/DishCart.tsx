185d4e335a9144bfb2666ac096087ca60163c760
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-16 00:56:10 +0600

    show empty state when carty is empty

--- src/containers/dishes/Home/DishCart/DishCart.tsx
+++ src/containers/dishes/Home/DishCart/DishCart.tsx
@@ -6,15 +6,44 @@ import DishCartItem from './DishCartItem/DishCartItem';
 const DishCart = () => {
   const cartDishes = useAppSelector(selectCartDish);
 
+  const renderContent = () => {
+    if (cartDishes.length === 0) {
+      return (
+        <>
+          <div className="empty-cart">
+            <div className="empty-cart-info">
+              <p className="empty-cart-info-text empty-cart-info-title">
+                your cart is empty
+              </p>
+              <p className="empty-cart-info-text empty-cart-info-description">
+                add pizza for happiness
+              </p>
+            </div>
+            <div className="empty-cart-image">
+              <img
+                src="/src/assets/images/placeholder/pizza-happy-ic.svg"
+                alt="empty cart :("
+              />
+            </div>
+          </div>
+        </>
+      );
+    }
+
+    return (
+      <>
+        {cartDishes.map((cart) => (
+          <DishCartItem cartDish={cart} />
+        ))}
+      </>
+    );
+  };
+
   return (
     <>
       <div className="dish-cart">
         <p className="dish-cart-title">your order</p>
-        <div className='dish-cart-block'>
-          {cartDishes.map((cart) => (
-            <DishCartItem cartDish={cart} />
-          ))}
-        </div>
+        <div className="dish-cart-block">{renderContent()}</div>
       </div>
     </>
   );
