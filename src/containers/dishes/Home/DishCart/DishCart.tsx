b739e0d7e990254dd63aa2f232b8730f095cc2fb
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-15 23:51:09 +0600

    add DishCart component to render cart items

---
+++ src/containers/dishes/Home/DishCart/DishCart.tsx
@@ -0,0 +1,21 @@
+import { selectCartDish } from '../../../../redux/features/dishes/dishCart/dishCart.selectors';
+import { useAppSelector } from '../../../../redux/hooks/reduxHooks';
+import './DishCart.css';
+import DishCartItem from './DishCartItem/DishCartItem';
+
+const DishCart = () => {
+  const cartDishes = useAppSelector(selectCartDish);
+
+  return (
+    <>
+      <div className="dish-cart">
+        <p className="dish-cart-title">your order</p>
+        {cartDishes.map((cart) => (
+          <DishCartItem cartDish={cart} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default DishCart;
